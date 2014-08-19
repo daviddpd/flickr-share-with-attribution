22aede11214003d6015a9461374899a8643067d7
David P. Discher <dpd@dpdtech.com>
2014-08-18 21:37:01 -0700

    inital import

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ flickr-share-with-attribution
 =============================
 
 Uses Flickr APIs to generate embedable HTML from photos Licensed under Creative Commons with attribution.
+
+This uses Phlickr for Flickr APIs. 
+http://sourceforge.net/projects/phlickr/
+
+If copying and pasting this into Word Press, Simple Custom CSS plugin will be helpful for adding
+the CCS needs for this blob.
+http://wordpress.org/plugins/simple-custom-css/
+
+You will also need a Flickr API Key.
+https://www.flickr.com/services/api/keys/
---
+++ flickr-share-with-attribution.php
@@ -0,0 +1,119 @@
+<?php
+
+$getFavs = 0;
+$getSinglePhoto = 0;
+$cid = -1;
+$user_id = '';
+
+if ($argc == 3 && in_array($argv[1], array('--id', '-id', ))) {
+	$getSinglePhoto = 1;
+	$cid = $argv[2];
+} elseif ($argc == 3 && in_array($argv[1], array('--user', '-user', ))) {
+
+	$getFavs = 1;
+	$user_id = $argv[2];
+
+} else {
+
+	print "usage --id or --user required\n";
+	exit;
+
+}
+
+require_once 'Phlickr/Api.php';
+if ( is_file ("flickr-apikey.php") ) {
+	include "flickr-apikey.php";
+} else {
+	define('FLICKR_API_KEY', '');
+	define('FLICKR_API_SECRET', '');
+	define('FLICKR_API_TOKEN', '');
+}
+
+
+// create an api
+$api = new Phlickr_Api(FLICKR_API_KEY, FLICKR_API_SECRET, FLICKR_API_TOKEN);
+
+$response = $api->executeMethod('flickr.photos.licenses.getInfo', array());
+$licenses_xml = simplexml_load_string($response);
+
+$licenses_array = array();
+
+foreach ($licenses_xml->licenses->{'license'} as $license ) {
+
+	$id= (int)$license{'id'};
+
+	$licenses_array[$id]['name'] = (string)$license{'name'};
+	$licenses_array[$id]['url']  = (string)$license{'url'};
+	
+	if ( preg_match ( '/creativecommons/', $licenses_array[$id]['url']  )  ) {
+		$a = explode  ( '/',  $licenses_array[$id]['url'] );
+		$licenses_array[$id]['cc'] = '<a href="' . $licenses_array[$id]['url'] . '">CC ' .  strtoupper ( $a[4] . " " . $a[5] ) . "</a>";
+	}
+
+}
+
+$response = '';
+$xml = '';
+$photos = null;
+
+if ( $getFavs ) {
+	print " ==== $user_id \n";
+	$response = $api->executeMethod('flickr.favorites.getList', array('user_id'=> "$user_id" ,'per_page' => 500));
+	$xml = simplexml_load_string($response);
+	$photos = $xml->photos->{'photo'};
+}
+
+if ( $getSinglePhoto ) {
+	$photos = array ();
+	$photos[0] = array  ( id => $cid );
+}
+
+foreach ($photos  as $photo) {
+	$response = $api->executeMethod('flickr.photos.getInfo', array('photo_id'=> $photo{'id'}));
+	$photo_xml = simplexml_load_string($response);
+
+	$response = $api->executeMethod('flickr.photos.getSizes', array('photo_id'=> $photo{'id'}));
+	$sizes_xml = simplexml_load_string($response);
+
+	$url = (string)$photo_xml->photo->urls->url;
+	$owner_url = "https://www.flickr.com/people/" . $photo_xml->photo->owner{'nsid'};
+	$owner_username = (string)$photo_xml->photo->owner{'username'};
+	$title = (string)$photo_xml->photo->{'title'};
+	$d = (string)$photo_xml->photo->{'description'};
+	$license = (int)$photo_xml->photo{'license'};
+	
+	if  ( ($license == 0) || ($license == 8) || ($license == 7) ) { continue; }
+
+	$sizes_array = array ();
+	
+	foreach ($sizes_xml->sizes->{'size'} as $size ){
+		$l = (string)$size{'label'};
+		if ( isset ( $sizes_array{$l}{'url'} ) ) { continue; }
+		
+		$sizes_array{$l}{'url'} = (string)$size{'source'};
+		$sizes_array{$l}{'width2'} = (int) ((int)$size{'width'} * 1.1 );
+		$sizes_array{$l}{'height'} = (int)$size{'height'};
+		$sizes_array{$l}{'height2'} = (int)((int)$size{'height'} * 1.3);
+	
+	}
+	
+?>
+
+<div class="flickrrow" style="height: <?= $sizes_array['Medium']['height2'] ?>;">
+	<div class="flickrimg" style="width: <?= $sizes_array['Medium']['width2'] ?>;">
+		<a target="_blank" href="<?= $url ?>"><img src="<?= $sizes_array['Medium']['url'] ?>" alt="<?= $title ." by " . $owner_username ?>" /></a>		
+	</div>
+	<div>
+		<div class="flickrtitle"><a target="_blank" href="<?= $url ?>"><?= $title ?></a> by <a target="_blank" href="<?= $owner_url ?>"><?= $owner_username ?></a></div>
+		<div class="flickrcredit">is licensed under <?= $licenses_array[$license]['cc']  ?> via Flickr</div>
+		<div class="flickrdesc"><?= $d ?></div>
+	</div>	
+	<div style="clear: both">&nbsp;</div>
+</div>
+<div>&nbsp;</div>
+
+
+<?php
+}
+
+?>
